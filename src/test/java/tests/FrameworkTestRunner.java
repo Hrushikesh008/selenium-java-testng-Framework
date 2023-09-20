232063847855b3606eac8a503a5981bb1acd4bed
Hrushikesh Suresh <hrushikesh.suresh@tcs.com>
2023-09-20 12:56:10 +0530

    Framework-v-1.3

--- src/test/java/tests/FrameworkTestRunner.java
+++ src/test/java/tests/FrameworkTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "src/test/resources/Features" }, glue = { "steps", "pages",
-		"utils" }, tags = "@Test", plugin = { "pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+		"utils" }, tags = "@Test", plugin = { "pretty", "html:target/cucumber.html", "json:target/cucumber/cucumber.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failed_scenarios.txt" }, monochrome = true, dryRun = false)
 
